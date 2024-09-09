2850c271edcb802bce8dbe38da92ff6fb80fee71
shakil <mdshakilhossain720@gmail.com>
2024-09-09 14:30:30 +0600

    error handle

---
+++ lib/audi_helper/services_locator.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:audio_service/audio_service.dart';
+import 'package:get_it/get_it.dart';
+
+import 'audi_handle.dart';
+import 'page_manager.dart';
+
+GetIt getIt = GetIt.instance;
+
+Future<void> setupServiceLocator() async {
+  getIt.registerSingleton<AudioHandler>( await initAudioService() );
+  getIt.registerLazySingleton<PageManager>( () => PageManager() );
+}
